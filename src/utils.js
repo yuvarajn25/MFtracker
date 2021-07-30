86c9d525f59bc3c385f637e0b76d285bc79e2a14
Yuvaraj Nithyanandan <yuvarajn25@gmail.com>
2021-07-30 11:05:29 +0530

    date formatted

--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,15 @@ import axios from "axios";
 import moment from "moment";
 import supabase from "./supabase";
 
+const DATE_FORMAT = "DD-MM-YYYY";
+
+function returnFormattedValue(navValues) {
+  return navValues.map((n) => ({
+    ...n,
+    date: moment.unix(n.date).format(DATE_FORMAT),
+  }));
+}
+
 export const fetchLastNav = async (schemeCode) => {
   const { data: navValues, error } = await supabase
     .from("navHistory")
@@ -9,17 +18,19 @@ export const fetchLastNav = async (schemeCode) => {
     .eq("schemeCode", schemeCode)
     .order("id", { ascending: false })
     .limit(2);
-
   if (navValues.length) {
     const now = moment();
-    if (now.isoWeekday() > 5) return navValues;
-    if (now.diff(moment.unix(navValues[0].date), "days") <= 1) return navValues;
+    if ([1, 7].includes(now.isoWeekday()))
+      return returnFormattedValue(navValues);
+
+    if (now.diff(moment.unix(navValues[0].date), "days") <= 1)
+      return returnFormattedValue(navValues);
   }
   const response = await axios.get(`https://api.mfapi.in/mf/${schemeCode}`);
-
+  const data = response?.data?.data.splice(0, 5);
   await supabase.from("navHistory").upsert(
-    response?.data?.data.splice(0, 5).map((d) => {
-      const date = moment(d.date, "DD-MM-YYYY").unix();
+    data.map((d) => {
+      const date = moment(d.date, DATE_FORMAT).unix();
       return {
         ...d,
         date,
@@ -28,5 +39,5 @@ export const fetchLastNav = async (schemeCode) => {
       };
     })
   );
-  return response?.data?.data.splice(0, 2);
+  return data.splice(0, 2);
 };
